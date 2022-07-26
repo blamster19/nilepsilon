923bc90e79c8b24f6cfd6dcb2113c99448fe6a75
blamster19 <blamster19@gmail.com>
2022-07-26 16:28:11 +0200

    test: add new `algebra` integration tests
    Test vector-scalar operations and norm methods.

--- tests/algebra_tests.rs
+++ tests/algebra_tests.rs
@@ -50,3 +50,50 @@ fn vector_vector_prod() {
 	let result = v1 % v2;
 	assert_eq!(result, v3);
 }
+
+#[test]
+fn vector_mul_by_scalar() {
+	let v3 = algebra::Vector {
+		x: 2.0,
+		y: 4.0,
+		z: 6.0,
+	};
+	let result = 2.0 * v1;
+	assert_eq!(result, v3);
+}
+
+#[test]
+fn vector_div_by_scalar() {
+	let v3 = algebra::Vector {
+		x: 0.5,
+		y: 1.0,
+		z: 1.5,
+	};
+	let result = v1 / 2.0;
+	assert_eq!(result, v3);
+}
+
+#[test]
+fn vector_norm() {
+	let v3 = (14.0 as algebra::Coord).sqrt();
+	let result = v1.norm();
+	assert_eq!(result, v3);
+}
+
+#[test]
+fn vector_norm_sqr() {
+	let v3 = 14.0;
+	let result = v1.norm_sqr();
+	assert_eq!(result, v3);
+}
+
+#[test]
+fn vector_normalize() {
+	let v3 = algebra::Vector {
+		x: 0.2672612419124244,
+		y: 0.5345224838248488,
+		z: 0.8017837257372732,
+	};
+	let result = v1.normalize();
+	assert_eq!(result, v3);
+}

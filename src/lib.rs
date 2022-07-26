4bcc730d6f52f4239f7720d56e639a16882768cb
blamster19 <blamster19@gmail.com>
2022-07-26 22:01:21 +0200

    feat: add `ray` module
    The module implements light ray structure.

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod output;
 pub mod algebra;
+pub mod ray;
---
+++ src/ray/mod.rs
@@ -0,0 +1,20 @@
+use crate::algebra;
+
+#[derive(Default, Clone, Debug, Copy)]
+pub struct Ray {
+	pub orig: algebra::Vector,
+	pub dir: algebra::Vector,
+}
+
+impl Ray {
+	pub fn new(orig: algebra::Vector, dir: algebra::Vector) -> Ray {
+		Ray {
+			orig,
+			dir,
+		}
+	}
+
+	pub fn point_on_line(&self, t: algebra::Coord) -> algebra::Vector {
+		self.orig + t * self.dir
+	}
+}

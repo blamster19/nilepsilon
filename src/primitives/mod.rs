73ce86f210d563cd110dad847e7f071299f9b473
blamster19 <blamster19@gmail.com>
2022-07-30 19:24:17 +0200

    refactor: make all primitives variant of one enum
    Achieved more elegant polymorphism with less boilerplate, as all primitives will have the same methods for ray intersection and normals anyway.

--- src/primitives/mod.rs
+++ src/primitives/mod.rs
@@ -6,18 +6,24 @@ enum Intersection {
 	Miss(algebra::Vector),
 }
 
-pub struct Sphere {
-	pub position: algebra::Vector,
-	pub radius: algebra::Scalar,
+pub enum Primitive {
+	Sphere {
+		position: algebra::Vector,
+		radius: algebra::Scalar,
+	}
 }
 
-impl Sphere {
+impl Primitive {
 	pub fn intersect(&self, ray: ray::Ray) -> bool {
-		let orig_to_center: algebra::Vector = ray.orig - self.position;
-		let a = ray.dir * ray.dir;
-		let b = 2.0 * orig_to_center * ray.dir;
-		let c = orig_to_center.norm_sqr() - self.radius * self.radius;
-		let delta = b * b - 4.0 * a * c;
-		delta > 0.0
+		match self {
+			Primitive::Sphere { position, radius } => {
+				let orig_to_center: algebra::Vector = ray.orig - *position;
+				let a = ray.dir * ray.dir;
+				let b = 2.0 * orig_to_center * ray.dir;
+				let c = orig_to_center.norm_sqr() - radius * radius;
+				let delta = b * b - 4.0 * a * c;
+				delta > 0.0
+			}
+		}
 	}
 }
--- src/scene/mod.rs
+++ src/scene/mod.rs
@@ -1,16 +1,12 @@
 use crate::primitives;
 use crate::camera;
 
-pub enum PrimitiveType {
-	Sphere(primitives::Sphere),
-}
-
 pub enum BackgroundType {
 	SolidColor((f64, f64, f64)),
 }
 
 pub struct Scene {
-	pub objects: Vec<PrimitiveType>,
+	pub objects: Vec<primitives::Primitive>,
 	pub camera: camera::Camera,
 	pub background: BackgroundType,
 }

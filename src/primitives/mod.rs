2c1e54e7530605da63ea1d151e99dd7bdd835dd0
blamster19 <blamster19@gmail.com>
2022-08-21 11:36:48 +0200

    fix: `Primitive` sphere point_on_surface method
    The method returned a point on unit sphere instead of a point on a sphere of given radius.

--- src/primitives/mod.rs
+++ src/primitives/mod.rs
@@ -172,7 +172,7 @@ impl Shape {
 
 	pub fn point_on_surface(&self, rand: (f64, f64, f64)) -> algebra::Vector {
 		match self {
-			Shape::Sphere { position, .. } => *position + algebra::Vector::new(rand.0, rand.1, rand.2),
+			Shape::Sphere { position, radius, .. } => *position + algebra::Vector::new(rand.0, rand.1, rand.2) * *radius,
 
 			Shape::Plane { position, .. } => *position,
 
